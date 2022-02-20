6eb351863ea6d1389c71439176ff604e206bb057
jannatul-mou149 <jannatul15-12149@gmail.com>
2022-02-20 21:44:51 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const name = document.getElementById('name').value;
+const name = document.getElementById('name').value;
+
+document.querySelector('#contact-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    e.target.elements.name.value = '';
+    e.target.elements.email.value = '';
+    e.target.elements.message.value = '';
+});
